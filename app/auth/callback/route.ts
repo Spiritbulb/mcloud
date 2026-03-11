3536b25713cbdc3ecff400b1f3f3d46500bdff2e
thatbush <bentley@spiritbulb.org>
2026-03-11 18:49:27 +0300

    callback issue

--- app/auth/callback/route.ts
+++
@@ -1,7 +0,0 @@
-// app/auth/callback/route.ts
-import { handleCallback } from '@/app/auth/actions'
-import { NextRequest } from 'next/server'
-
-export async function GET(request: NextRequest) {
-    return handleCallback(request.nextUrl.searchParams)
-}
